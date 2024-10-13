2ffc323b2899172e2e80349ec8e282c8d6e44052
JorgeGallegos03 <145165745+JorgeGallegos03@users.noreply.github.com>
2024-10-12 20:56:43 -0500

    Update Lp2T2JorgeGallegosApplication.java

--- src/main/java/com/cibertec/LP2_T2_JorgeGallegos/Lp2T2JorgeGallegosApplication.java
+++ src/main/java/com/cibertec/LP2_T2_JorgeGallegos/Lp2T2JorgeGallegosApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lp2T2JorgeGallegosApplication {
+public class Lp2T2JorgeGallegosApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lp2T2JorgeGallegosApplication.class, args);
