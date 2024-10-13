cda28f02662b7bf23cc86f62679a4fca1eee3da0
JorgeGallegos03 <145165745+JorgeGallegos03@users.noreply.github.com>
2024-10-12 21:00:02 -0500

    Update EmpleadoRepository.java

--- src/main/java/com/cibertec/LP2_T2_JorgeGallegos/repository/EmpleadoRepository.java
+++ src/main/java/com/cibertec/LP2_T2_JorgeGallegos/repository/EmpleadoRepository.java
@@ -2,7 +2,7 @@ package com.cibertec.LP2_T2_JorgeGallegos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cibertec.LP2_T2_JorgeGallegos.models.EmpleadoClass;
+import com.cibertec.LP2_T2_JorgeGallegos.models.EmpleadoClass; 
 
 public interface EmpleadoRepository extends JpaRepository<EmpleadoClass, String> {
 	EmpleadoClass findByDniEmpleado(String dniEmpleado);
