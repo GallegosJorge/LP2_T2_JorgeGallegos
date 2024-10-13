a5fa66b48a3eb16fa6a9c9588083136c753d954d
JorgeGallegos03 <145165745+JorgeGallegos03@users.noreply.github.com>
2024-10-12 21:00:14 -0500

    Update AreaRepository.java

--- src/main/java/com/cibertec/LP2_T2_JorgeGallegos/repository/AreaRepository.java
+++ src/main/java/com/cibertec/LP2_T2_JorgeGallegos/repository/AreaRepository.java
@@ -2,7 +2,7 @@ package com.cibertec.LP2_T2_JorgeGallegos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cibertec.LP2_T2_JorgeGallegos.models.AreaClass;
+import com.cibertec.LP2_T2_JorgeGallegos.models.AreaClass; 
 
 public interface AreaRepository  extends JpaRepository<AreaClass, Integer>{
 
